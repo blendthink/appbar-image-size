af318cbb0367b3ac56923c0ad4465aabbed2c98f
Tatsuya Okayama (blendthink) <admin@blendthink.dev>
2024-12-19 22:50:05 +0900

    refactor main app structure and add custom AppBar with images

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const _App());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class _App extends StatelessWidget {
+  const _App();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      home: _Home(),
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.cyan,
+        ),
+      ),
+    );
+  }
+}
+
+class _Home extends StatelessWidget {
+  const _Home();
+
+  @override
+  Widget build(BuildContext context) {
+    const divider = Divider(
+      height: 0.5,
+    );
+    const title = Text('AppBar Image Size');
+    final image = Image.asset(
+      'assets/dash.png',
+      color: Colors.black,
+      width: 32,
+      height: 32,
+    );
+
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            divider,
+            AppBar(
+              primary: false,
+              title: title,
+              leading: image,
+            ),
+            divider,
+            AppBar(
+              primary: false,
+              title: title,
+              leading: Center(
+                child: image,
+              ),
+            ),
+            divider,
+          ],
         ),
       ),
     );
